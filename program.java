202a52419b48fbdcd0e2c098ac9a79db02318db7
Munaza077 <113824405+Munaza077@users.noreply.github.com>
2024-05-21 09:15:13 +0500

    Update program.java

--- program.java
+++ program.java
@@ -1,6 +1,6 @@
 public class program{
     public static void main(string[] args){
-        System.out.println("Munaza");
+        System.out.println("Munaza Dar");
         System.out.println("Assignment 04");
 
     }
